5a2d1910af58bf28c232e4e9c7cac6fe3b48f5f4
Eriksson <HERIKS19@volvocars.com>
2021-09-01 10:42:43 +0200

    Revert "Update radar_interface.py"
    This reverts commit 2e7213629317d480c78bbdcbf134cf2c51429460.

--- selfdrive/car/toyota/radar_interface.py
+++ selfdrive/car/toyota/radar_interface.py
@@ -25,4 +25,80 @@ def _create_radar_can_parser(car_fingerprint):
   return CANParser(DBC[car_fingerprint]['radar'], signals, checks, 1)
 
 class RadarInterface(RadarInterfaceBase):
-  pass
+  def __init__(self, CP):
+    super().__init__(CP)
+    self.track_id = 0
+    self.radar_ts = CP.radarTimeStep
+
+    if CP.carFingerprint in TSS2_CAR:
+      self.RADAR_A_MSGS = list(range(0x180, 0x190))
+      self.RADAR_B_MSGS = list(range(0x190, 0x1a0))
+    else:
+      self.RADAR_A_MSGS = list(range(0x210, 0x220))
+      self.RADAR_B_MSGS = list(range(0x220, 0x230))
+
+    self.valid_cnt = {key: 0 for key in self.RADAR_A_MSGS}
+
+    self.rcp = _create_radar_can_parser(CP.carFingerprint)
+    self.trigger_msg = self.RADAR_B_MSGS[-1]
+    self.updated_messages = set()
+
+    # No radar dbc for cars without DSU which are not TSS 2.0
+    # TODO: make a adas dbc file for dsu-less models
+    self.no_radar = CP.carFingerprint in NO_DSU_CAR and CP.carFingerprint not in TSS2_CAR
+
+  def update(self, can_strings):
+    if self.no_radar:
+      return super().update(None)
+
+    vls = self.rcp.update_strings(can_strings)
+    self.updated_messages.update(vls)
+
+    if self.trigger_msg not in self.updated_messages:
+      return None
+
+    rr = self._update(self.updated_messages)
+    self.updated_messages.clear()
+
+    return rr
+
+  def _update(self, updated_messages):
+    ret = car.RadarData.new_message()
+    errors = []
+    if not self.rcp.can_valid:
+      errors.append("canError")
+    errors = []
+    ret.errors = errors
+
+    for ii in sorted(updated_messages):
+      if ii in self.RADAR_A_MSGS:
+        cpt = self.rcp.vl[ii]
+
+        if cpt['LONG_DIST'] >= 255 or cpt['NEW_TRACK']:
+          self.valid_cnt[ii] = 0    # reset counter
+        if cpt['VALID'] and cpt['LONG_DIST'] < 255:
+          self.valid_cnt[ii] += 1
+        else:
+          self.valid_cnt[ii] = max(self.valid_cnt[ii] - 1, 0)
+
+        score = self.rcp.vl[ii+16]['SCORE']
+        # print ii, self.valid_cnt[ii], score, cpt['VALID'], cpt['LONG_DIST'], cpt['LAT_DIST']
+
+        # radar point only valid if it's a valid measurement and score is above 50
+        if cpt['VALID'] or (score > 50 and cpt['LONG_DIST'] < 255 and self.valid_cnt[ii] > 0):
+          if ii not in self.pts or cpt['NEW_TRACK']:
+            self.pts[ii] = car.RadarData.RadarPoint.new_message()
+            self.pts[ii].trackId = self.track_id
+            self.track_id += 1
+          self.pts[ii].dRel = cpt['LONG_DIST']  # from front of car
+          self.pts[ii].yRel = -cpt['LAT_DIST']  # in car frame's y axis, left is positive
+          self.pts[ii].vRel = cpt['REL_SPEED']
+          self.pts[ii].aRel = float('nan')
+          self.pts[ii].yvRel = float('nan')
+          self.pts[ii].measured = bool(cpt['VALID'])
+        else:
+          if ii in self.pts:
+            del self.pts[ii]
+
+    ret.points = list(self.pts.values())
+    return ret
