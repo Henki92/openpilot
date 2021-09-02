ebe4553b6568dbef4114eed2ca79ac50f896ae6a
Henki92 <henrik_eriksson92@hotmail.com>
2021-09-02 09:50:30 +0200

    Update carstate.py

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -31,10 +31,13 @@ class CarState(CarStateBase):
     ret.seatbeltUnlatched = cp.vl["SEATS_DOORS"]["SEATBELT_DRIVER_UNLATCHED"] != 0
 
     ret.brakePressed = cp.vl["BRAKE_MODULE"]["BRAKE_PRESSED"] != 0
+    print("Brake pressed: ", ret.brakePressed)
     if self.CP.enableGasInterceptor:
+      print("Hello1")
       ret.gas = (cp.vl["GAS_SENSOR"]["INTERCEPTOR_GAS"] + cp.vl["GAS_SENSOR"]["INTERCEPTOR_GAS2"]) / 2.
       ret.gasPressed = ret.gas > 15
     else:
+      print("Hello2")
       ret.gas = cp.vl["GAS_PEDAL"]["GAS_PEDAL"]
       ret.gasPressed = cp.vl["PCM_CRUISE"]["GAS_RELEASED"] == 0
 
