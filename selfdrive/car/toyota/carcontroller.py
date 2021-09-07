98c74f420dcad3b21b2fa70c4ca0edb035a2e1b1
Eriksson <HERIKS19@volvocars.com>
2021-09-07 11:12:52 +0200

    Update carcontroller.py
    Usual print debugs

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -99,6 +99,7 @@ class CarController():
     # sending it at 100Hz seem to allow a higher rate limit, as the rate limit seems imposed
     # on consecutive messages
     can_sends.append(create_steer_command(self.packer, apply_steer, apply_steer_req, frame))
+    print("Sending steer command: ", apply_steer, apply_steer_req, frame)
     if frame % 2 == 0 and CS.CP.carFingerprint in TSS2_CAR:
       can_sends.append(create_lta_steer_command(self.packer, 0, 0, frame // 2))
 
@@ -115,8 +116,10 @@ class CarController():
       if pcm_cancel_cmd and CS.CP.carFingerprint == CAR.LEXUS_IS:
         can_sends.append(create_acc_cancel_command(self.packer))
       elif CS.CP.openpilotLongitudinalControl:
+        print("If 1 accel_command", pcm_accel_cmd, pcm_cancel_cmd, self.standstill_req, lead, CS.acc_type)
         can_sends.append(create_accel_command(self.packer, pcm_accel_cmd, pcm_cancel_cmd, self.standstill_req, lead, CS.acc_type))
       else:
+        print("If 2 accel_command", pcm_cancel_cmd, lead, CS.acc_type)
         can_sends.append(create_accel_command(self.packer, 0, pcm_cancel_cmd, False, lead, CS.acc_type))
 
     if frame % 2 == 0 and CS.CP.enableGasInterceptor:
